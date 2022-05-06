bbfec697aca5ad96832fb2cbbfa8cb6352000aae
pashnik <pashnik98@gmail.com>
2022-05-06 11:29:23 +0300

    t

--- install-zsh-stuff.sh
+++ install-zsh-stuff.sh
@@ -8,6 +8,8 @@ git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 mkdir -p ~/.zsh/plugins/zsh-syntax-highlighting
 mv zsh-syntax-highlighting ~/.zsh/plugins/
 
+
+
 ## spaceship promt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
