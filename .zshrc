237ddb12dcfd6934e68dda492105211591e79310
pashnik <pashnik98@gmail.com>
2024-04-11 17:59:50 +0200

    comment on zsh installation

--- .zshrc
+++ .zshrc
@@ -1,10 +1,15 @@
+# Set GOPATH
 export GOPATH=$HOME/go
+
+# Add GOPATH/bin to PATH
 export PATH=$PATH:$GOPATH/bin
 
+# Path to oh-my-zsh configuration
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="spaceship"
 
+# ZSH plugins
 plugins=(git docker golang sbt scala)
 
 source $ZSH/oh-my-zsh.sh
--- install-zsh-stuff.sh
+++ install-zsh-stuff.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
-## oh-my-zsh
+# Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-## zsh syntax highlighting
+# Install ZSH syntax highlighting
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 mkdir -p ~/.zsh/plugins/zsh-syntax-highlighting
 mv zsh-syntax-highlighting ~/.zsh/plugins/
 
-## spaceship promt
+# Install Spaceship Promt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
